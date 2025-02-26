a895462efbf5e97f140896396d9100288991e80c
ahollard <ahollard26@bfhsla.org>
2025-02-25 18:01:13 -0600

    finished are you being served

--- iot-projects/are-you-being-served/raceServer.js
+++ iot-projects/are-you-being-served/raceServer.js
@@ -20,14 +20,34 @@ http.createServer(function(req, res) {
     }
 
     // TODO 12: Make the whole thing parallel
-    async.series( 
-        // TODO 9: Supply an array of functions
+    async.parallel( 
+        // TODO 8: Supply an array of functions
         [
-            
+            function (callback) {
+                wrapper(callback);
+              },
+              function (callback) {
+                wrapper(callback);
+              },
+              function (callback) {
+                wrapper(callback);
+              },
+              function (callback) {
+                wrapper(callback);
+              },
         ],
         function (error, results) {
             // TODO 10: add a callback function to the end of the async call to tally the results 
-
+            res.write("Results:\n");
+            var victoryOrder = sortTogether(racers, results);
+            for (let i = 0; i < victoryOrder.length; i++) {
+                res.write(victoryOrder[i] + "\n");
+            }
+            let d = new Date();
+            let endTime = d.getTime();
+            let duration = endTime - startTime;
+            res.write("Race Duration: " + duration + "ms\n");
+        res.end();
         }
     );
     
