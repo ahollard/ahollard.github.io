d5fa031b0ce0967f120ca8e996c71dcbb713ffc8
ahollard <ahollard26@bfhsla.org>
2025-02-25 19:58:22 -0600

    todo 6 still broken

--- iot-projects/catch-server/safeServer.js
+++ iot-projects/catch-server/safeServer.js
@@ -11,13 +11,15 @@ function requestListener(req, res) {
             var body = "";
             req.on("data", function(data) {
                body += data;
+               console.log(body)
+
             })
             req.on("end", function() {
                 serverStatus = {};
                 serverStatus.status = JSON.parse(body);
-                res.writeHead(200, {"Content-Type": "plain/text"});
-                res.write("The server has been updated.")
             })
+            res.writeHead(200, {"Content-Type": "plain/text"});
+                res.write("The server has been updated.")
         }
     } catch(error) {
         res.writeHead(500, {"Content-Type": "test/plain"});
